ed9e503bd8767c6ef8257ee866b944ffce712ea2
J.Son <x@x-wx.com>
2017-09-20 11:51:39 +0800

    不转换app.js文件的图片路径

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 /**
  * Created by JSon on 2017/6/22.
  */
-import path from 'path'
-import fs from 'fs'
-import mime from 'mime'
+import path from 'path';
+import fs from 'fs';
+import mime from 'mime';
 
 export default class {
 
-  constructor (c = {}) {
+  constructor(c = {}) {
     this.setting = Object.assign({}, {
       css: true,
       html: false
-    }, c)
+    }, c);
   }
 
-  apply (op) {
-    const {code} = op
-    if (code) {
-      const reg = /(\/\w+)+\w+\.(png|jpg|gif|jpeg)/gi
-      const imgPaths = code.match(reg) || []
+  apply(op) {
+    const { code, file } = op;
+    if (code && !/app\.js/.test(file)) {
+      const reg = /(\.?)+(\/\w+)+\w+\.(png|jpg|gif|jpeg)/gi;
+      const imgPaths = code.match(reg) || [];
       imgPaths.map(imgPath => {
-        const pathFile = path.join(process.cwd(), imgPath)
+        const pathFile = path.join(process.cwd(), this.setting.path || '', imgPath);
         try {
-          const bData = fs.readFileSync(pathFile)
-          const base64Str = bData.toString('base64')
-          const mimeType = mime.lookup(pathFile)
-          const dataUri = `data:${mimeType};base64,${base64Str}`
-          op.code = op.code.replace(imgPath, `${dataUri}`)
-        } catch(e) {
-          console.error('读取图片失败:', pathFile)
+          const bData = fs.readFileSync(pathFile);
+          const base64Str = bData.toString('base64');
+          const mimeType = mime.lookup(pathFile);
+          const dataUri = `data:${mimeType};base64,${base64Str}`;
+          op.code = op.code.replace(imgPath, `${dataUri}`);
+        } catch (e) {
+          console.error('读取图片失败:', pathFile);
         }
-      })
+      });
     }
-    op.next()
+    op.next();
   }
 }
